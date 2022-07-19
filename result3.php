d11f788a33c036779f8e863ad623667d5411097c
Peter Chen <peterchen2017125@gmail.com>
2022-07-19 19:22:57 +0800

    20220719

---
+++ result3.php
@@ -0,0 +1,9 @@
+<?php
+    if(isset($_POST['username'])){
+        // isset 判斷變數是否已存在有就回傳true
+        $user = $_POST['username'];
+        $sex = $_POST['sex'];
+        $edu = $_POST['edu'];
+        
+    }
+?>
