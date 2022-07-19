a1ccb7ffb4c985787f218fcca79b3db076af6e66
Peter Chen <peterchen2017125@gmail.com>
2022-07-19 20:19:35 +0800

    20220719

---
+++ tabledemo.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table{
+            border: 1px solid #000;
+            width: 90%;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>姓名</th>
+            <th>年齡</th>
+            <th>學校</th>
+        </tr>
+        <?php
+            for($i = 0; $i <= 10; $i ++){
+        ?>
+        <tr>
+            <td>阿剛</td>
+            <td>30</td>
+            <td>台東大學</td>
+        </tr>
+        <?php}?>
+    </table>
+</body>
+</html>
