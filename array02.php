187a0f2db3d4f94277c805e760ea66e88d4e0376
Peter Chen <peterchen2017125@gmail.com>
2022-07-21 20:08:17 +0800

    20220721

---
+++ array02.php
@@ -0,0 +1,7 @@
+<?php
+$city = array("tp" => "taipei" , "tc" => "taichune" "kh" => "kouhousn");
+echo $city["tc"] . "<br>";
+
+$area = array(2 => "新北市區域" , 4 => "中部區域" , 6 => "南部區域");
+echo $area[6] . "<br>";
+?>
